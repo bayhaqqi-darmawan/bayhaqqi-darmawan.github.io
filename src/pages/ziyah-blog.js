1e036a9d16d402acd7dafd06d26249097fd77c9d
Bayhaqqi <18ftt1707@student.pb.edu.bn>
2020-04-11 01:29:42 +0800

    complete ziyah's blog page

--- src/pages/ziyah-blog.js
+++ src/pages/ziyah-blog.js
@@ -16,7 +16,6 @@ const ZiyahPage = () => {
       id
       childImageSharp {
         fixed (
-          width: 300,
           height: 250) {
           ...GatsbyImageSharpFixed
         }
@@ -30,7 +29,84 @@ const ZiyahPage = () => {
       id
       childImageSharp {
         fixed (
-          width: 300,
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image3:
+    file(relativePath: {eq: "sab1.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image4:
+    file(relativePath: {eq: "sab2.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image5:
+    file(relativePath: {eq: "revenge1.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image6:
+    file(relativePath: {eq: "revenge2.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image7:
+    file(relativePath: {eq: "dev1.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
+          height: 250) {
+          ...GatsbyImageSharpFixed
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    image8:
+    file(relativePath: {eq: "dev2.jpg"}) {
+      id
+      childImageSharp {
+        fixed (
           height: 250) {
           ...GatsbyImageSharpFixed
         }
@@ -73,21 +149,112 @@ const ZiyahPage = () => {
         <Row lg="2" xs="1">
           <Col>
             <Img 
-              fixed = {data1.image1.childImageSharp.fixed}
+              fluid = {data1.image1.childImageSharp.fluid}
             />
           </Col>
 
           <Col>
           <Img 
-              fixed = {data1.image2.childImageSharp.fixed}
+              fluid = {data1.image2.childImageSharp.fluid}
+            />
+          </Col>
+        </Row>
+        
+        <br></br>
+        <br></br>
+        <Row lg="1" xs="1">
+        <Col>
+        <p className="title">
+          2. Switched at birth
+        </p>
+        </Col>
+
+        <Col>
+        <p>
+        This American series family drama premiered in 2011. Each episode is an hour long or less but trust me it is definitely worth every second! This show revolves around two teenagers born on the same day but was accidentally switched between their families by an overworked nurse. I’d recommend this show to those interested in sign language as this show includes a deaf female character that plays as one of the switched teen. What I love most about this show is how it is able to change my point of view or my perspective about people with hearing loss.
+        </p>
+        
+        </Col>
+
+        </Row>
+
+        <Row lg="2" xs="1">
+          <Col>
+            <Img 
+              fluid = {data1.image3.childImageSharp.fluid}
+            />
+          </Col>
+
+          <Col>
+          <Img 
+              fluid = {data1.image4.childImageSharp.fluid}
             />
           </Col>
         </Row>
 
-        
-      
+        <br></br>
+        <br></br>
+        <Row lg="1" xs="1">
+        <Col>
+        <p className="title">
+          3. Revenge
+        </p>
+        </Col>
 
-      
+        <Col>
+        <p>
+        This television series was inspired by a novel named The Count of Monte Cristo. This show I’d recommend for those who love mysteries and thrill because this show is considered dark. This show is about a young woman whose father was framed for treason by a wealthy family. After being in a juvenile correctional facility, she now returns to get revenge on those who wronged her and her father. 
+        </p>
+        
+        </Col>
+
+        </Row>
+
+        <Row lg="2" xs="1">
+          <Col>
+            <Img 
+              fluid = {data1.image5.childImageSharp.fluid}
+            />
+          </Col>
+
+          <Col>
+          <Img 
+              fluid = {data1.image6.childImageSharp.fluid}
+            />
+          </Col>
+        </Row>
+        
+        <br></br>
+        <br></br>
+        <Row lg="1" xs="1">
+        <Col>
+        <p className="title">
+          4. Devious Maids
+        </p>
+        </Col>
+
+        <Col>
+        <p>
+        Four Latina maids working for the area’s wealthiest and most powerful families discover secrets, mysteries and even crimes hidden behind these families. With a touch of romance and comedy, this show is very entertaining to watch throughout all four of the seasons. Each episode is almost an hour long filled with various events happening but is definitely fun to keep up with the drama and the gossips between the maids.
+        </p>
+        
+        </Col>
+
+        </Row>
+
+        <Row lg="2" xs="1">
+          <Col>
+            <Img 
+              fluid = {data1.image7.childImageSharp.fluid}
+            />
+          </Col>
+
+          <Col>
+          <Img 
+              fluid = {data1.image8.childImageSharp.fluid}
+            />
+          </Col>
+        </Row>
     </Container>
   </Layout>
 )
