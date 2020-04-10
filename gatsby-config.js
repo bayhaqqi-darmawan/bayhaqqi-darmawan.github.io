a71ab70d38fc4d6ef058cad10eabdadeb716e276
Bayhaqqi <18ftt1707@student.pb.edu.bn>
2020-04-10 22:30:55 +0800

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `BayZee Blog`,
+        short_name: `BZ Blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
   pathPrefix: "/bayhaqqi-darmawan.github.io"
 }
